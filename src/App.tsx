fd9c5b71c5e650271fae37d57aa6a4204db95317
Kyle Nazario <knazario@bitovi.com>
2022-01-13 16:51:35 -0500

    Loads category on request

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {queryWolframAlpha} from "./WolframAlpha.connector";
 
 function App() {
+  const [categoryInfo, setCategoryInfo] = useState('')
+
+    const loadCategory = (category = 'best actress', year = 2020) => {
+      queryWolframAlpha(category, year).then(setCategoryInfo).catch(setCategoryInfo)
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppHeader />
+      <button onClick={() => loadCategory()}>Get Random Category</button>
+        <p>{categoryInfo}</p>
     </div>
   );
+}
+
+function AppHeader() {
+  return (
+    <header className="App-header">
+      <h1>Random Oscars</h1>
+      <p>Hit the button to get a random Oscar category from a random year</p>
+    </header>
+  )
 }
 
 export default App;

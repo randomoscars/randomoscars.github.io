a78d6b3b2b0e3c084c0c254e4d7de3d319185c51
Kyle Nazario <knazario@bitovi.com>
2022-01-13 17:14:35 -0500

    Randomizers for category and year

--- src/Models.ts
+++ src/Models.ts
@@ -68,3 +68,35 @@ export interface Sources {
   url: string;
   text: string;
 }
+
+export interface OscarCategory {
+  name: string;
+  code: string;
+}
+
+export const categories = [
+  "Best Actor in a Leading Role",
+  "Best Actor in a Supporting Role",
+  "best supporting actor",
+  "Best Actress in a Leading Role",
+  "Best Adapted Screenplay",
+  "Best Animated Feature",
+  "Best Art Direction",
+  "Best Cinematography",
+  "Best Costume Design",
+  "Best Director",
+  "Best Documentary Feature",
+  "Best Documentary Short Subject",
+  "Best Film Editing",
+  "Best Foreign Language Film",
+  "Best Makeup",
+  "Best Original Score",
+  "Best Original Screenplay",
+  "Best Original Song",
+  "Best Original Story",
+  "Best Picture",
+  "Best Sound Editing",
+  "Best Sound Mixing",
+  "Best Visual Effects",
+  "Live Action Short Film",
+];
---
+++ src/Randomizer.ts
@@ -0,0 +1,10 @@
+import { categories } from "./Models";
+
+export function getRandomCategory(): string {
+  const randomIndex = Math.floor(Math.random() * categories.length);
+  return categories[randomIndex];
+}
+
+export function getRandomYear(): number {
+  return Math.floor(Math.random() * 92) + 1929;
+}
